d50b118ee60ca7d18c44d375380ca6e2b024e520
Caio Carvalho <caio.cmalves@gmail.com>
2023-04-17 20:17:16 -0300

    criação da classe lutador

---
+++ src/UltraEmojiCombat.java
@@ -0,0 +1,10 @@
+
+import classes.Lutador;
+
+public class UltraEmojiCombat {
+    public static void main(String[] args) throws Exception {
+        Lutador l1 = new Lutador("Pretty Boy", "França", 32, 1.76, 75, 12, 5, 0);
+
+
+    }
+}
